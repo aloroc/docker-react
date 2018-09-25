172d25a0551885ac217f303fe47277b96a07885c
root <aloroc82@gmail.com>
2018-09-25 17:14:38 +0200

    changed app text in FB

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-        Welcome to JR's awesome react app
+        Change in feature branch
         </p>
       </div>
     );
